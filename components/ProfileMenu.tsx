4cb114528dac042051c0dfc6442c515b9e67f3a3
Michael <9247037+nifte@users.noreply.github.com>
2022-07-21 20:22:22 -0700

    increase profile menu z index

--- components/ProfileMenu.tsx
+++ components/ProfileMenu.tsx
@@ -35,7 +35,7 @@ const ProfileMenu = () => {
 				leaveFrom="transform opacity-100 scale-100"
 				leaveTo="transform opacity-0 scale-95"
 			>
-				<Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+				<Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
 					{session && (
 						<Menu.Item disabled>
 							<span className="block px-4 py-2 text-sm text-gray-700 overflow-ellipsis">
